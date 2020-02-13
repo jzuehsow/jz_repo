f2e4ba8494bf78ed683e9180cc8bb0f27ad45857
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2020-02-13 09:29:59 -0500

    Update common.ps1

--- common.ps1
+++ common.ps1
@@ -1,1 +1,23 @@
 
+Function Start_Script
+{
+  Import-Module ActiveDirectory
+  Remove-Variable * -Force -ErrorAction SilentlyContinue
+  $psHost = Get-Host
+  $psWindow = $psHost.UI.RawUI
+  $newSize = $psWindow.BufferSize
+  $newSize.Height = 65
+  $newSize.Width = 120
+  $psWindow.BufferSize = $newSize
+  $domain = Get-ADDomain
+  $Script:enclave = $domain.NetBIOSName
+  $Script:pdc = $domain.PDCEmulator
+  $Script:domain = $domain.DNSRoot
+  $Script:date = Get-Date -F yyyy-MM-dd
+  $Script:year = Get-Date -F yyyy
+  $Script:month = Get-Date -F MM
+  $Script:monthFN = Get-Date -F MMMM
+  $Script:day = Get-Date -F dd
+  $Script:dayFN = Get-Date -F ddd
+  $Script:smtp = "[SMTP.DOMAIN.COM]"
+}
