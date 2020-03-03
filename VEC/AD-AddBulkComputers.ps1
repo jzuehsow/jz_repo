a31e98a9a0dc45e7f5c7c5e1af2d5f7ab079ee17
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2020-03-03 10:05:39 -0500

    Update AD-AddBulkComputers.ps1

--- VEC/AD-AddBulkComputers.ps1
+++ VEC/AD-AddBulkComputers.ps1
@@ -1,12 +1,32 @@
+<###############################################################################################################################
+
+Created by: Jeremy Zuehsow
+
+Summary: Import CSV of new computers to add to AD environment.
+
+v1.1 - Added comments to variables; Modified way the script imports CSV and creates computer objects
+
+###############################################################################################################################>
 
 
+Set-Location $PSScriptRoot
+.".\Config\Common.ps1"
+$version = '1.1'
 
-Import-Module ActiveDirectory
-$ErrorActionPreference = 'SilentlyContinue'
+Start_Script
+
+#IF STATEMENT TO GET CSV; MANDATORY CSV HEADERS?
 $csv = '.\NewComputers.csv'
+
+#IF STATEMENT TO TARGET OU
 $ou = 'OU=COMPUTERS,OU=HELPDESK,OU=SITE,OU=REGION,DC=MICROSOFT,DC=CONTOSO,DC=COM'
+
+#OPTIONAL
 $vlan = '8021x_VLAN'
 
-Import-Csv -Path $csv | ForEach -Object {New-ADComputer -Name $_.ComputerAccount -Description $_.ComputerDescription -Path $ou -Enabled $false}
-$computers = Get-ADComputer -Filter * -SearchBase $ou | Select -ExpandProperty SamAccountName
-Add-ADGroupMember $vlan -Members $computers
+$newComputers = Import-Csv -Path $csv
+ForEach ($computer in $newComputers)
+{
+    New-ADComputer $_.ComputerName -Description $_.Description -Path $computersOU -Enabled $false
+    Add-ADGroupMember $vlan -Members $computer
+}
