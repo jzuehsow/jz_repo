757d54b34d540b5bfb985da01635c77cfdff4e1d
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2021-06-29 18:42:54 -0400

    Update Bulk-NoChallenge.ps1
    Added header box and variable formatting

--- Windows/DHA/New Class/Bulk-NoChallenge.ps1
+++ Windows/DHA/New Class/Bulk-NoChallenge.ps1
@@ -1,13 +1,28 @@
+<#############################################################################################################################################################
+
+Author - Jeremy Zuehsow
+Purpose - Add bulk users from csv to MFA exemption
+
+Change Log:
+v2.1 - Modifications to script per Exchange team request
 
 
 
+#############################################################################################################################################################>
 
-$csv = import-csv "[NEW CLASS CSV LIST]"
 
-Foreach ($line in $csv)
+$csvPath = "[NEW CLASS CSV LIST]"
+$MFAExemptGroup = '[MFA EXEMPT GROUP]'
+
+$csv = Import-Csv $csvPath
+
+ForEach ($rmLine in $csv)
 {
-$sam = $line.SamAccountName
-$usr = get-aduser $sam
-set-aduser $usr -replace @{Attribute1="RSA_PERM"}
-set-aduser $usr -replace @{Attribute1Description="MFA Exempt"}
-Add-ADGroupMember WKS-RSA-NoChallenge-Perm $usr}
+    $rmSam = $rmLine.SamAccountName
+    $rmUser = Get-ADUser $rmSam
+    Set-ADUser $rmUser -Replace @{Attribute1="RSA_PERM"}
+    Set-ADUser $rmUser -Replace @{Attribute1Description="MFA Exempt"}
+    Add-ADGroupMember $MFAExemptGroup $rmUser
+
+    Remove-Variable -Name "rm*" -Force
+}
