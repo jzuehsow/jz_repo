e0ece08e4cb7e685627ff7102f574e38e066bef4
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2024-03-20 15:33:38 -0400

    Add morpheus/dpf

---
+++ Morpheus/runDPF.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#Define Variables
+NOW="$(date '+%Y%m%d-%H:%M:%S')"
+MORPHEUS_ROOT=/workspace
+ingestFolder="/morpheus/ingest"
+ingestFiles=()
+for filePath in "$ingestFolder"/*.csv; do
+        filename=$(basename "$filePath")
+        echo $filename
+        ingestFiles+=("$filename")
+done
+echo $ingestFiles; exit
+
+
+#Stop and remove all containers
+for name in $(podman ps --format "{{.Names}}"); do
+        echo -e "\nStopping container: $name"
+        podman stop "$name"
+        podman wait "$name"
+done
+for name in $(podman ps -a --format "{{.Names}}"); do
+        echo -e "\nRemoving container: $name"
+        podman rm "$name"
+done
+
+
+#Start trtion container
+echo -e "\n\nStarting triton container with log-parsing-onnx model"
+podman run -d --name triton --device nvidia.com/gpu=0 --gpus=1 --security-opt=label=disable \
+        -p 0.0.0.0:8000:8000 -p 0.0.0.0:8001:8001 -p 0.0.0.0:8002:8002 \
+        -v /morpheus/models:/models:Z nvcr.io/nvidia/tritonserver:23.07-py3 \
+                tritonserver --model-repository=/models/triton-model-repo \
+                --exit-on-error=false \
+                --model-control-mode=explicit \
+                --load-model log-parsing-onnx
+
+#Start morpheus-logs container
+echo -e "\n\nStarting morpheus-logs container with $ingestFile"
+podman run -d --name morpheus-logs -v /morpheus/ingest:/ingest:Z --device nvidia.com/gpu=0 --gpus=1 --security-opt=label=disable nvcr.io/nvidia/morpheus/morpheus:23.07-runtime \
+        python ${MORPHEUS_ROOT}/examples/log_parsing/run.py \
+        --num_threads 1 \
+        --input_file /ingest/$ingestFile \
+        --output_file ./log-parsing-output.jsonlines \
+        --model_vocab_hash_file=${MORPHEUS_ROOT}/models/data/bert-base-cased-hash.txt \
+        --model_vocab_file=${MORPHEUS_ROOT}/models/training-tuning-scripts/sid-models/resources/bert-base-cased-vocab.txt \
+        --model_seq_length=256 \
+        --model_name log-parsing-onnx \
+        --model_config_file=${MORPHEUS_ROOT}/models/log-parsing-models/log-parsing-config-20220418.json \
+        --server_url 192.171.4.249:8001
+
+#Copy json output from morpheus container
+while true; do
+        if ! podman ps --filter "name=morpheus-logs" --filter "status=running" | grep -q 'morpheus-logs'; then
+                podman cp morpheus-logs:/workspace/log-parsing-output.jsonlines ./log-parsing-output-${NOW}.jsonlines
+                break
+        fi
+        sleep 1
+done
