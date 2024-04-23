5538be0b2431d905497dac082d2270b5f4ca0bfd
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2024-04-23 01:18:36 -0400

    Update trainDFPv2.sh

--- Morpheus/trainDFPv2.sh
+++ Morpheus/trainDFPv2.sh
@@ -8,11 +8,6 @@ trainer=morpheus-trainer
 inference=morpheus-inference
 containerOpts="--device nvidia.com/gpu=0 --gpus=1 --security-opt=label=disable"
 
-morpheusServer="python /workspace/examples/digital_fingerprinting/starter/run_cloudtrail_dfp.py \
---columns_file=${modelsDir}/data/columns_ae_cloudtrail.txt \
---input_glob=${models}/datasets/validation-data/dfp-cloudtrail-*-input.csv"
-trainerPipeline="${morpheusServer} --train_data_glob=${modelsDir}/datasets/training-data/dfp-*.csv --models_output_filename=${modelsDir}/dfp-models/${outputModel} --output_file ./${outputFile}"
-inferencePipeline="${morpheusServer} --pretrained_filename=/ingest/${outputModel} --output_file ./${outputFile}"
 outputModel=cloudtrail_ae_user_models.pkl
 outputFile=cloudtrail-dfp-results.csv
 
@@ -29,6 +24,7 @@ container_check () {
   fi
 }
 
+
 container_check $triton
 podman run -d --name $triton -v /morpheus/models:/models:Z -p 0.0.0.0:8000:8000 -p 0.0.0.0:8001:8001 -p 0.0.0.0:8002:8002 $containerOpts $tritonVersion \
   tritonserver \
@@ -38,12 +34,21 @@ podman run -d --name $triton -v /morpheus/models:/models:Z -p 0.0.0.0:8000:8000 
 
 container_check $trainer
 podman run -d --name $trainer -v /morpheus/ingest:/ingest:Z $containerOpts $morpheusVersion \
-  $trainerPipeline
+  python /workspace/examples/digital_fingerprinting/starter/run_cloudtrail_dfp.py \
+  --columns_file=${modelsDir}/data/columns_ae_cloudtrail.txt \
+  --input_glob=${modelsDir}/datasets/validation-data/dfp-cloudtrail-*-input.csv \
+  --train_data_glob=${modelsDir}/datasets/training-data/dfp-*.csv \
+  --models_output_filename=${modelsDir}/dfp-models/${outputModel} \
+  --output_file ./${outputFile}
 copy_file $trainer
 
 container_check $inference
 podman run -d --name $inference -v /morpheus/ingest:/ingest:Z $containerOpts $morpheusVersion \
-  $inferencePipeline
+  python /workspace/examples/digital_fingerprinting/starter/run_cloudtrail_dfp.py \
+  --columns_file=${modelsDir}/data/columns_ae_cloudtrail.txt \
+  --input_glob=${modelsDir}/datasets/validation-data/dfp-cloudtrail-*-input.csv \
+  --pretrained_filename=/ingest/${outputModel} \
+  --output_file ./${outputFile}
 copy_file $inference
 
 
