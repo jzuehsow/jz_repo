70084a24dcc76270785280e2a7bbd66783255184
jzuehsow <57189589+jzuehsow@users.noreply.github.com>
2024-04-25 01:20:01 -0400

    Update trainDFPv2.sh
    Add copy_file and insert_timestamp functions

--- Morpheus/trainDFPv2.sh
+++ Morpheus/trainDFPv2.sh
@@ -7,11 +7,11 @@ triton=triton
 trainer=morpheus-trainer
 inference=morpheus-inference
 containerOpts="--device nvidia.com/gpu=0 --gpus=1 --security-opt=label=disable"
-
 outputModel=cloudtrail_ae_user_models.pkl
 outputFile=cloudtrail-dfp-results.csv
 
 container_check () {
+  local containerName="$1"
   status=$(podman inspect "$containerName" --format '{{.State.Status}}' 2>/dev/null)
   if [[ "$status" == "running" ]]; then
     echo "\n\n${containerName} is running."
@@ -24,6 +24,26 @@ container_check () {
   fi
 }
 
+copy_file () {
+  local containerName="$1"
+  local file="$2"
+  while true; do
+  if ! podman ps --filter "name=${containerName}" --filter "status=running" | grep -q ${containerName}; then
+    newFile=$(insert_timestamp "$file")
+    podman cp ${containerName}:/workspace/$file ./$newFile
+    break
+  fi
+  sleep 1
+  done
+}
+
+insert_timestamp () {
+  local filename="$1"
+  local timestamp="$(date '+%Y%m%d-%H:%M:%S')"
+  local extension="${filename##*.}"
+  local fileNameOnly="${filename%.*}"
+  local newFilename=${fileNameOnly}-${timestamp}.${extension}"
+}
 
 container_check $triton
 podman run -d --name $triton -v /morpheus/models:/models:Z -p 0.0.0.0:8000:8000 -p 0.0.0.0:8001:8001 -p 0.0.0.0:8002:8002 $containerOpts $tritonVersion \
@@ -36,11 +56,11 @@ container_check $trainer
 podman run -d --name $trainer -v /morpheus/ingest:/ingest:Z $containerOpts $morpheusVersion \
   python /workspace/examples/digital_fingerprinting/starter/run_cloudtrail_dfp.py \
   --columns_file=${modelsDir}/data/columns_ae_cloudtrail.txt \
-  --input_glob=${modelsDir}/datasets/validation-data/dfp-cloudtrail-*-input.csv \
+  --input_glob=${modelsDir}/datasets/validation-data/dfp-cloudtrail-*-input.csv \ #N/A for Model?
   --train_data_glob=${modelsDir}/datasets/training-data/dfp-*.csv \
   --models_output_filename=${modelsDir}/dfp-models/${outputModel} \
-  --output_file ./${outputFile}
-copy_file $trainer
+  --output_file ./${outputFile} #N/A for Model?
+copy_file $trainer $outputModel
 
 container_check $inference
 podman run -d --name $inference -v /morpheus/ingest:/ingest:Z $containerOpts $morpheusVersion \
@@ -49,20 +69,11 @@ podman run -d --name $inference -v /morpheus/ingest:/ingest:Z $containerOpts $mo
   --input_glob=${modelsDir}/datasets/validation-data/dfp-cloudtrail-*-input.csv \
   --pretrained_filename=/ingest/${outputModel} \
   --output_file ./${outputFile}
-copy_file $inference
+copy_file $inference $outputFile
 
 
 
-copy_file () {
-  while true; do
-  if ! podman ps --filter "name=${containerName}" --filter "status=running" | grep -q ${containerName}; then
-    timestamp="$(date '+%Y%m%d-%H:%M:%S')"
-    podman cp ${containerName}:<PATH> <LOCAL PATH>
-    break
-  fi
-  sleep 1
-  done
-}
+
 
 podman cp ${trainer}:${modelsDir}/dfp-models/${outputModel} ./cloudtrail_ae_user_models-${timestamp}.pkl
 podman cp ${trainer}:/workspace/${outputFile} ./cloudtrail-dfp-results-${timestamp}.csv
